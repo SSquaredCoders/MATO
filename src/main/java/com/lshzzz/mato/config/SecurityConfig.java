06c3d3be8cab2702eb14a1f65d77e9e9ca73cf82
JEESW <jeesw1221@gmail.com>
2025-03-18 11:07:10 +0900

    refactor: JWT Access Token을 저장하기 위해 CORS 설정에 Authorization 추가

--- src/main/java/com/lshzzz/mato/config/SecurityConfig.java
+++ src/main/java/com/lshzzz/mato/config/SecurityConfig.java
@@ -62,6 +62,7 @@ public class SecurityConfig {
         corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         corsConfiguration.setAllowedHeaders(List.of("*"));
         corsConfiguration.setAllowCredentials(true); // 쿠키 등 자격 증명을 사용하는 경우
+        corsConfiguration.setExposedHeaders(List.of("Authorization", "Cookie"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfiguration);

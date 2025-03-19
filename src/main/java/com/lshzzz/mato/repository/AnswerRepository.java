b90483fb4c61cc79bc762e40f55441f818965a08
LLSNsssz <seungjang93@gmail.com>
2025-03-20 01:03:52 +0900

    Feat: AnswerRepository, HintRepository 추가 - Song ID 기준으로 정답 및 힌트 조회 기능 구현

---
+++ src/main/java/com/lshzzz/mato/repository/AnswerRepository.java
@@ -0,0 +1,10 @@
+package com.lshzzz.mato.repository;
+
+import com.lshzzz.mato.model.song.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
+	List<Answer> findBySongId(Long songId);
+}

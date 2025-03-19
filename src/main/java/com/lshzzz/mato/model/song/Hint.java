bc324bfd585f5db86c92dec400e4ef1e84c1f3c6
LLSNsssz <seungjang93@gmail.com>
2025-03-19 23:29:33 +0900

    Feat: Hint 엔티티 추가 - Song과 연관된 힌트(Hint) 엔티티 추가 - 특정 시간(hintTime) 이후에 힌트 표시 기능 구현 가능

---
+++ src/main/java/com/lshzzz/mato/model/song/Hint.java
@@ -0,0 +1,40 @@
+package com.lshzzz.mato.model.song;
+
+import com.lshzzz.mato.model.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "hints")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class Hint extends BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY) // 🎯 하나의 노래에 여러 개의 힌트 가능
+	@JoinColumn(name = "song_id", nullable = false)
+	private Song song;
+
+	@Column(nullable = false)
+	private String hintText; // 🎯 힌트 내용 (ex. "OST", "3글자")
+
+	@Column(nullable = false)
+	private int hintTime; // 🎯 몇 초 후에 힌트를 보여줄 것인지 설정 (ex. 10초 후)
+}

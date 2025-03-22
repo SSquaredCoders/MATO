dfa3b4a82c0e78022e93f4b9b60b623fb4b5abb8
LLSNsssz <seungjang93@gmail.com>
2025-03-22 22:56:12 +0900

    Refactor: HintRequestDto에 mapSongId 필드 추가 - 어떤 맵송(MapSong)에 속한 힌트인지 명확하게 전달하기 위함 - HintData 내부 구조는 그대로 유지

--- src/main/java/com/lshzzz/mato/model/song/dto/HintRequestDto.java
+++ src/main/java/com/lshzzz/mato/model/song/dto/HintRequestDto.java
@@ -3,6 +3,7 @@ package com.lshzzz.mato.model.song.dto;
 import java.util.List;
 
 public record HintRequestDto(
+	Long mapSongId,
 	List<HintData> hints
 ) {
 	public record HintData(String hintText, int revealTime) {}

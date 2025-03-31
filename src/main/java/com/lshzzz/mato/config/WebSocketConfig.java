0b978296f74b3feb9b4f9cd3e27b161c8f225a7b
LeeSH <seungjang93@gmail.com>
2025-03-31 11:54:24 +0900

    Chore: CORS 및 WebSocket 허용 도메인에 배포 서버 IP 추가 - CORS 설정에 http://112.159.76.59 추가 - WebSocket 엔드포인트에도 동일한 IP 허용 도메인으로 등록

--- src/main/java/com/lshzzz/mato/config/WebSocketConfig.java
+++ src/main/java/com/lshzzz/mato/config/WebSocketConfig.java
@@ -19,7 +19,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/ws")
-			.setAllowedOrigins("http://localhost:3000", "http://localhost:8080", "http://localhost:5173") // 특정 도메인만 허용
+			.setAllowedOrigins("http://localhost:3000", "http://localhost:8080", "http://localhost:5173", "http://112.159.76.59:5173", "http://112.159.76.59:8081"
+			) // 특정 도메인만 허용
 			.withSockJS();
 	}
 

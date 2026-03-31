a98bfe86ea805c48c0ff2e02119f61b265c4fd14
LeeSH <seungjang93@gmail.com>
2026-03-31 20:27:27 +0900

    test: pipeline auto build trigger test

--- src/main/java/com/lshzzz/mato/MatoApplication.java
+++ src/main/java/com/lshzzz/mato/MatoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+// Pipeline test - auto build trigger
 @SpringBootApplication
 @EnableScheduling
 public class MatoApplication {
